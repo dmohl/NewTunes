0ebdda2f043a3182c5784a8e1345e5e5afa485c8
Dan Mohl <dmohl@yahoo.com>
2011-01-30 17:22:00 -0600

    Added images

--- NewTunes/App/ViewModels/ItemViewModel.fs
+++ NewTunes/App/ViewModels/ItemViewModel.fs
@@ -9,15 +9,13 @@ type ItemViewModel() =
     let mutable artistId = ""
     let mutable artistName = ""
     let mutable collectionName = ""
-    let mutable artistViewUrl = ""
+    let mutable artworkUrl100 = ""
     let mutable releaseDate = ""
-    let mutable collectionViewUrl = ""
     [<DataMember(Name="artistId")>] member x.ArtistId with get() = artistId and set(v) = artistId <- v
     [<DataMember(Name="artistName")>] member x.ArtistName with get() = artistName and set(v) = artistName <- v
     [<DataMember(Name="collectionName")>] member x.CollectionName with get() = collectionName and set(v) = collectionName <- v
-    [<DataMember(Name="artistViewUrl")>] member x.ArtistViewUrl with get() = artistViewUrl and set(v) = artistViewUrl <- v
+    [<DataMember(Name="artworkUrl100")>] member x.ArtworkUrl100 with get() = artworkUrl100 and set(v) = artworkUrl100 <- v
     [<DataMember(Name="releaseDate")>] member x.ReleaseDate with get() = releaseDate and set(v) = releaseDate <- v
-    [<DataMember(Name="collectionViewUrl")>] member x.CollectionViewUrl with get() = collectionViewUrl and set(v) = collectionViewUrl <- v
     
 [<DataContract()>]
 type iTunesJsonResults() =
